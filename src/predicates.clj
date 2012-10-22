9a8891dcaf671cfa1dd167388833a74c06599951
Juhana Laurinharju <fingerzam@gmail.com>
2012-10-23 01:11:43 +0300

    Predicates

---
+++ src/predicates.clj
@@ -0,0 +1,41 @@
+(ns predicates)
+
+(defn sum-f [f g x]
+  :-)
+
+(defn less-than [n]
+  :-)
+
+(defn equal-to [n]
+  :-)
+
+(defn set->predicate [a-set]
+  :-)
+
+(defn pred-and [pred1 pred2]
+  :-)
+
+(defn pred-or [pred1 pred2]
+  :-)
+
+(defn whitespace? [character]
+  (Character/isWhitespace character))
+
+(defn blank? [string]
+  :-)
+
+(defn has-award? [book award]
+  :-)
+
+(defn HAS-ALL-THE-AWARDS? [book awards]
+  :-)
+
+(defn my-some [pred a-seq]
+  :-)
+
+(defn my-every? [pred a-seq]
+  :-)
+
+(defn prime? [n]
+  :-)
+;^^
