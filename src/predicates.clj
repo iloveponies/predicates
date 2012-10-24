4264c5627a3cdad029ec8d1670d315bfc6c7db3a
Kalle Lammenoja <kalle.lammenoja@gmail.com>
2012-10-24 05:00:25 +0300

    some done

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,34 +1,38 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (if(and (= nil x) (contains? a-set nil))
+			(set nil)
+            (a-set x))))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn[x](and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn[x](or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (if (or (= nil string) (every? whitespace? string) (= "" string))
+    true
+    false))
 
 (defn has-award? [book award]
-  :-)
+  )
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  )
 
 (defn my-some [pred a-seq]
   :-)
@@ -38,4 +42,3 @@
 
 (defn prime? [n]
   :-)
-;^^
