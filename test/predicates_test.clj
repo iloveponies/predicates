74b07b9243dd52e0f1782d90a8ea5171809f5de0
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2012-10-26 14:27:49 +0300

    Log entry 404: 'The unicorns are finally taking over.

--- test/predicates_test.clj
+++ test/predicates_test.clj
@@ -36,22 +36,31 @@
 (def awards #{:locus, :world-fantasy, :hugo})
 
 (def china {:name "China Miéville", :birth-year 1972})
+
 (def octavia {:name "Octavia E. Butler"
               :birth-year 1947
               :death-year 2006})
+
 (def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+
 (def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+
 (def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
 
 (def authors #{china, octavia, kay, dick, zelazny})
 
 (def cities {:title "The City and the City" :authors #{china}
              :awards #{:locus, :world-fantasy, :hugo}})
+
 (def wild-seed {:title "Wild Seed", :authors #{octavia}})
+
 (def lord-of-light {:title "Lord of Light", :authors #{zelazny}
                     :awards #{:hugo}})
+
 (def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
+
 (def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
+
 (def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
 
 (def books #{cities, wild-seed, lord-of-light,
