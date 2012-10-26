c1a61556ffb796640185ab8d806595cb45903107
Antti Rautanen <grilladroid@gmail.com>
2012-10-26 12:44:44 +0300

    Exercises 1-6

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,28 +1,38 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
-(defn set->predicate [a-set]
-  :-)
+(defn set->predicate [needle]
+  (fn [haystack]
+    (contains? needle haystack)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x]
+    (and
+      (pred1 x)
+      (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x]
+    (or
+      (pred1 x)
+      (pred2 x))))
+
+(defn whitespace? [character]
+  (Character/isWhitespace character))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
   :-)
