f324569b2949b415ca6f36170d065d6eeb8c1e2b
Eetu Nylander <eetu.nylander@gmail.com>
2012-10-28 22:45:44 +0200

    tehty osa

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,19 +1,21 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (let [alku (filter a-set [0 2 4 6 nil])
+        loppu (filter nil? [0 2 4 6 nil])]
+     (conj alku loppu)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  ( pred1 pred2))
 
 (defn pred-or [pred1 pred2]
   :-)
@@ -25,17 +27,20 @@
   :-)
 
 (defn has-award? [book award]
-  :-)
+  (if (contains? (set (map :awards book)) award)
+    true
+    false))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
   :-)
 
 (defn my-some [pred a-seq]
-  :-)
+  (if (nil? (some pred a-seq))
+    false
+    true))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (every? pred a-seq))
 
 (defn prime? [n]
   :-)
-;^^
