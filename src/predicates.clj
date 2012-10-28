ddbea1987fbfaf819c5da610c8a872fcef99dbd6
Ville <ville@vih-99.(none)>
2012-10-28 19:04:48 +0200

    osa tehty

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,34 +1,34 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== n k)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (and (set->predicate book) (contains? (:awards book) award))) 
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (fn [award] (every? (contains? awards award) (:awards book))))
 
 (defn my-some [pred a-seq]
   :-)
@@ -38,4 +38,3 @@
 
 (defn prime? [n]
   :-)
-;^^
