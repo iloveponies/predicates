22970a88ada12d6eed28ccf11a609c59805278a6
Jani Rahkola <jani.rahkola@gmail.com>
2013-03-09 13:41:37 +0200

    numbered tests

--- test/predicates_test.clj
+++ test/predicates_test.clj
@@ -2,33 +2,36 @@
   (:use predicates
         midje.sweet))
 
-(facts "sum-f"
+(facts "1 sum-f"
   (sum-f inc dec 4)      => 8
   (sum-f inc identity 5) => 11
   (sum-f identity - 10)  => 0)
 
-(facts "less-than"
+(facts "2 less-than"
   (filter (less-than 3) [1 2 3 4 5])   => [1 2]
   (filter (less-than 4) [-2 12 3 4 0]) => [-2 3 0])
 
-(facts "equal-to"
+(facts "2 equal-to"
   (filter (equal-to 2) [2 1 3 2.0]) => [2 2.0]
   (filter (equal-to 2) [3 4 5 6])   => [])
 
-(facts "set->predicate"
+(facts "3 set->predicate"
   (filter (set->predicate #{1 2 3})     [0 2 4 6])       => [2]
   (filter (set->predicate #{1 2 3 nil}) [2 nil 4 nil 6]) => [2 nil nil])
 
-(facts "pred-and"
-  (filter (pred-and pos? even?) [1 2 -4 0 6 7 -3]) => [2 6]
-  (filter (pred-and pos? odd?) [1 2 -4 0 6 7 -3])  => [1 7]
-  (filter (pred-and (complement nil?) empty?) [[] '() nil {} #{}]) => [[] '() {} #{}])
+(facts "4 pred-and"
+  (filter (pred-and pos? even?) [1 2 -4 0 6 7 -3])
+  => [2 6]
+  (filter (pred-and pos? odd?) [1 2 -4 0 6 7 -3])
+  => [1 7]
+  (filter (pred-and (complement nil?) empty?) [[] '() nil {} #{}])
+  => [[] '() {} #{}])
 
-(facts "pred-or"
+(facts "5 pred-or"
   (filter (pred-or pos? odd?) [1 2 -4 0 6 7 -3])  => [1 2 6 7 -3]
   (filter (pred-or pos? even?) [1 2 -4 0 6 7 -3]) => [1 2 -4 0 6 7])
 
-(facts "blank?"
+(facts "6 blank?"
   (blank? " \t\n\t ") => true
   (blank? "  \t a")   => false
   (blank? "")         => true)
@@ -57,11 +60,11 @@
 (def books #{cities, wild-seed, lord-of-light,
              deus-irae, ysabel, scanner-darkly})
 
-(facts "has-award?"
+(facts "7 has-award?"
   (has-award? ysabel :world-fantasy) => true
   (has-award? scanner-darkly :hugo)  => false)
 
-(facts "HAS-ALL-THE-AWARDS?"
+(facts "8 HAS-ALL-THE-AWARDS?"
   (HAS-ALL-THE-AWARDS? cities #{:locus})
   => true
   (HAS-ALL-THE-AWARDS? cities #{:locus :world-fantasy :hugo})
@@ -75,7 +78,7 @@
   (HAS-ALL-THE-AWARDS? scanner-darkly #{})
   => true)
 
-(facts "my-some"
+(facts "9 my-some"
   (my-some even? [1 3 5 7])       => falsey
   (my-some even? [1 3 5 7 8])     => true
   (my-some neg? [1 3 5 0 7 8])    => falsey
@@ -86,13 +89,13 @@
   (my-some nil? [1 2])            => falsey
   (my-some nil? [1 nil 2])        => true)
 
-(facts "my-every?"
+(facts "10 my-every?"
   (my-every? pos? [1 2 3 4])   => true
   (my-every? pos? [1 2 3 4 0]) => false
   (my-every? even? [2 4 6])    => true
   (my-every? even? [])         => true)
 
-(facts "prime?"
+(facts "11 prime?"
   (prime? 4)                   => false
   (prime? 7)                   => true
   (prime? 10)                   => false
