48164d2b974c92d442c15014ce313afb12b87c44
Pii A J Sainio <pisapisa@dhp-67.cs.helsinki.fi>
2013-10-25 15:43:08 +0300

    palautus

--- src/predicates.clj
+++ src/predicates.clj
@@ -26,17 +26,22 @@
       (every? whitespace? string)))
 
 (defn has-award? [book award]
-  )
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [award? (fn [award] (has-award? book award))]
+    (every? award? awards)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [true-values (filter pred a-seq)]
+    (if (not (empty? true-values))
+    (pred (first true-values))
+    false)))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (== (count a-seq) (count (filter pred a-seq))))
 
 (defn prime? [n]
-  :-)
+  (let [divides? (fn [x] (== 0 (mod n x)))]
+    (not (some divides? (range 2 n)))))
 ;^^
