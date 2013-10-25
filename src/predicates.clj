0380cc1209997f68bba318686422d4a124ec8436
Teemu Sarapisto <tsarpf@gmail.com>
2013-10-25 17:11:02 +0300

    a few exercises done

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,34 +1,43 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n))
+  )
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x))
+  )
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (cond
+   (empty? string) true
+   (= string nil) true
+   (every? whitespace? string) true
+   :else false))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
+
+(defn hazaward [book]
+  (fn [award] (contains? (:awards book) award)))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (hazaward book) awards))
 
 (defn my-some [pred a-seq]
   :-)
