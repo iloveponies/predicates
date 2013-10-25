e225c1e89652b477caaacc8ee14ae4d9dc54e222
Mikael <->
2013-10-25 14:11:00 +0300

    -

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,28 +1,33 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and
+           (pred1 x)
+           (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or
+           (pred1 x)
+           (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string)
+  )
 
 (defn has-award? [book award]
   :-)
@@ -31,10 +36,18 @@
   :-)
 
 (defn my-some [pred a-seq]
-  :-)
+  (if (empty? a-seq)
+  false
+    (if (first (map pred a-seq))
+      (first (map pred a-seq))
+      (my-some pred (rest a-seq)))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (if (empty? a-seq)
+  true
+    (if (first (map pred a-seq))
+      (my-every? pred (rest a-seq))
+      false)))
 
 (defn prime? [n]
   :-)
