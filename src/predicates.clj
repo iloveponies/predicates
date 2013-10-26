dfa0d5690c9d7a933e34c6867d64d3a6ad28f25f
laurihy <lauri.hynynen@gmail.com>
2013-10-26 13:14:49 -0700

    done

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,53 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (g x) (f x)))
 
 (defn less-than [n]
-  :-)
+  (fn [x] (< x n) ))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== x n) ))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x) ))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (empty? string)
+      (every? whitespace? string)
+  ))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (get book :awards) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [x] (has-award? book x)) awards ))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (filter (fn [x] (boolean x)) (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (cond
+    (empty? a-seq) true
+    (pred (first a-seq)) (my-every? pred (rest a-seq))
+    :else false
+    ))
 
-(defn prime? [n]
-  :-)
+(defn asddivides? [n x]
+  (== (mod n x) 0))
+
+(defn prime? [n] (
+  let [divides? (fn [x] (== (mod n x) 0))]
+  (empty? (filter divides? (range 2 n)))
+  )
+)
 ;^^
