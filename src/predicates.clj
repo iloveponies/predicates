ab64d16658fecde0246db98321eb3ea79879816b
Teemu Hurme <teemu.hurme@cs.helsinki.fi>
2013-10-27 03:18:16 +0300

    pred

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,50 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n) ))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [n] (contains? a-set n)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+ (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or ( empty? string) (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn[award] (has-award? book award)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (cond
+  (empty? a-seq) false
+  (pred (first a-seq)) (pred (first a-seq))
+  :else (my-some pred (rest a-seq))
+  ))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (cond
+  (empty? a-seq) true
+  (pred (first a-seq)) (my-every? pred (rest a-seq))
+  :else false
+  ))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn[d] (integer? (/ n d) ))]
+    (not (some pred (range 2 n)))))
 ;^^
