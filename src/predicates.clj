dd4057f1f2ad15f8ef24a39fd04c68fc967438f4
Vaenae <jvannine@gmail.com>
2014-01-19 18:47:49 +0200

    first attempt

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,47 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (if (contains? a-set x) true false)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  #(and (pred1 %) (pred2 %)))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  #(or (pred1 %) (pred2 %)))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  ((set->predicate (:awards book)) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? #(has-award? book %) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [res
+  (first (map #(if (nil? %) true %)
+       (filter pred a-seq)))]
+    (if (vector? res)
+      (first res)
+      (if res true nil))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (= a-seq (filter pred a-seq)))
 
 (defn prime? [n]
-  :-)
+  (let [pred #(= 0 (mod n %))]
+    (not (some pred (range 2 n)))))
 ;^^
