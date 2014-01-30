7df3623d090f390854272a47b5a6db478bf070ed
Gregg Williams <gregg13@innerpaths.net>
2014-01-29 20:28:46 -0800

    passes all tests

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject predicates "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [iloveponies.tests/predicates "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+                 [iloveponies.tests/predicates "0.1.0-SNAPSHOT"]
+                  [midje "1.6.0"]]
+;  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}}
+  :plugins [[lein-midje "3.1.3"]])
--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,53 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [x] (< x n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== x n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [a-member] (contains? a-set a-member)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [a-val]
+    (and
+     (pred1 a-val)
+     (pred2 a-val))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [a-val]
+    (or
+     (pred1 a-val)
+     (pred2 a-val))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (and
+   (not (nil? (book :awards)))
+   (not (nil? (contains? (book :awards) award)))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (not-any? false?
+   (map
+    (fn [award] (contains? (book :awards) award))
+    awards)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (filter (complement false?) (map pred a-seq))))
+
 
 (defn my-every? [pred a-seq]
-  :-)
+  (reduce #(and % %2) true (map pred a-seq)))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred (fn [x] (zero? (rem n x)))]
+    (not (some pred (range 2 n)))))
