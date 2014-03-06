006f9e36575a6cc594ee462bea144972dbba566c
Chris Duncan <celldee@gmail.com>
2014-03-06 21:36:51 +0000

    All systems go.

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,50 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x)
+               (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x)
+              (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (empty? string)
+      (nil? string)
+      (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (set->predicate (:awards book)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (filter #(not (= % false)) (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [bool-responses (set (map pred a-seq))]
+    (cond
+      (empty? a-seq) true
+      (contains? bool-responses false) false
+      :else true)))
 
 (defn prime? [n]
-  :-)
+  (let [pred #(= (mod n %) 0 )]
+    (not (some pred (range 2 n)))))
 ;^^
