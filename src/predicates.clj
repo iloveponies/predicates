9f4c7503b1ad2731ef0937784cb05fbfcec7782b
Neil McCalum <neil.mccallum@telefab.com>
2014-03-26 05:34:32 +1300

    predicates solutions

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,41 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== n k)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [a-map] (contains? a-set a-map)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn[ x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (not (nil? (award (:awards book)))))
 
-(defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+defn HAS-ALL-THE-AWARDS? [book awards]
+  (every? (fn [award] (has-award? book award)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+   (not (empty? (filter pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (empty? (filter (complement pred) a-seq)))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred (fn [div] (= 0 (mod n div)))]
+    (not (some pred (range 2 n)))))
