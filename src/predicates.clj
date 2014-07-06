56d50e33bf47291f46b10039f06f973f05dfadda
David Hovemeyer <david.hovemeyer@gmail.com>
2014-07-06 07:41:15 -0400

    implemented my-some function

--- src/predicates.clj
+++ src/predicates.clj
@@ -33,7 +33,10 @@
   (every? (fn [award] (has-award? book award)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [candidates (filter pred a-seq)]
+    (if (empty? candidates)
+      false
+      (pred (first candidates)))))
 
 (defn my-every? [pred a-seq]
   :-)
