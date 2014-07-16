426f55fcded9170673ab216a61e5d1ae63c96c23
Matteo Traina <matteo.traina.mail@gmail.com>
2014-07-16 17:50:44 +0100

    first

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,34 +1,74 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or pred1 x) (pred2 x)))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
+
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+(def authors #{china, octavia, kay, dick, zelazny})
+
+(def cities {:title "The City and the City" :authors #{china}
+             :awards #{:locus, :world-fantasy, :hugo}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
+                    :awards #{:hugo}})
+(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
+(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
+(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
+
+(def books #{cities, wild-seed, lord-of-light,
+             deus-irae, ysabel, scanner-darkly})
 
 (defn has-award? [book award]
-  :-)
+  ((complement nil?) (award (:awards book))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  ;(=
+   ;(awards
+    (:awards book)
+    ;)
+   ;)
+  )
+
+(HAS-ALL-THE-AWARDS? cities #{:locus})
+;=> true
+(HAS-ALL-THE-AWARDS? cities #{:locus :world-fantasy :hugo})
+;=> true
+(HAS-ALL-THE-AWARDS? cities #{:locus :world-fantasy :hugo :pulitzer})
+;=> false
+(HAS-ALL-THE-AWARDS? lord-of-light #{:locus :world-fantasy :hugo})
+;=> false
+(HAS-ALL-THE-AWARDS? lord-of-light #{:hugo})
+;=> true
+(HAS-ALL-THE-AWARDS? scanner-darkly #{})
+;=> true
 
 (defn my-some [pred a-seq]
   :-)
