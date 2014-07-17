ae774d11b886f9508a77d5b18ba31f0cc4c822d5
Matteo Traina <matteo.traina.mail@gmail.com>
2014-07-17 10:37:57 +0100

    third

--- src/predicates.clj
+++ src/predicates.clj
@@ -16,7 +16,7 @@
   (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  (fn [x] (or pred1 x) (pred2 x)))
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
@@ -24,40 +24,25 @@
 (defn blank? [string]
   (every? whitespace? string))
 
-(def china {:name "China Miéville", :birth-year 1972})
-(def octavia {:name "Octavia E. Butler"
-              :birth-year 1947
-              :death-year 2006})
-(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
-(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
-(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
-
-(def authors #{china, octavia, kay, dick, zelazny})
-
-(def cities {:title "The City and the City" :authors #{china}
-             :awards #{:locus, :world-fantasy, :hugo}})
-(def wild-seed {:title "Wild Seed", :authors #{octavia}})
-(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
-                    :awards #{:hugo}})
-(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
-(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
-(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
-
-(def books #{cities, wild-seed, lord-of-light,
-             deus-irae, ysabel, scanner-darkly})
-
 (defn has-award? [book award]
   ((complement nil?) (award (:awards book))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
   (every? #(has-award? book %) awards))
 
+(defn filtered-seq [pred a-seq]
+  (filter #(not= false %) (map #(pred %) a-seq)))
+
 (defn my-some [pred a-seq]
-  :-)
+  (let [fst (first (filter #(not= false %) (map #(pred %) a-seq)))]
+    (if fst fst false)))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [a-seq-size (count a-seq)]
+    (= a-seq-size (count (filtered-seq pred a-seq)))))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (= 0 (mod n x)))]
+    (not (some pred (range 2 n)))))
+
 ;^^
