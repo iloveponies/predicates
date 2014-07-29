1806df8040e67a9979cc25a84aa29b1691b8e98a
Grzegorz Jacykowski <nickt@o2.pl>
2014-07-29 19:42:35 +0200

    Temporary solution of 50%

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,31 +1,33 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+ (+ (f x) (g x)) )
 
 (defn less-than [n]
-  :-)
+  (fn [x] (< x n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== x n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (if (award (:awards book)) 
+    true 
+    false))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
   :-)
