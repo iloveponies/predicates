f0b753bb407ea070835356e58b5b789ed44620cc
Simon Tuohey <simon@tuohey.net>
2014-08-08 18:30:28 +1000

    checking awards

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,31 +1,35 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x)(g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [k] (contains? a-set k)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [k]
+    (and (pred1 k)
+         (pred2 k))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [k]
+    (or (pred1 k)
+        (pred2 k))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (fn [k] (filter book (set->predicate (:awards book) award))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
   :-)
@@ -38,4 +42,4 @@
 
 (defn prime? [n]
   :-)
-;^^
+
