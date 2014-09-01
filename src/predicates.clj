25d1080b5899a423c111d2935a8740736a760bb7
raarh <arto.rantanen@helsinki.fi>
2014-09-01 19:20:15 +0300

    all done

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,45 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x ) (g x)))
+
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n )))
 
 (defn equal-to [n]
-  :-)
+    (fn [k] (== k n )))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [a] (true? (contains? a-set a))))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [a-map] (and (pred1 a-map) (pred2 a-map))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [a-map] (or (pred1 a-map) (pred2 a-map))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (get book :awards) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [award] (has-award? book award)) awards))
+
 
 (defn my-some [pred a-seq]
-  :-)
+  (some pred a-seq))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (every? pred a-seq))
+
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [div (fn [x] (not (== 0 (mod n x))))]
+    (every? div (range 2  n))))
+;^^ °~°
