358ba49c89176c3b6ed66f8763db5e03ef0cd59d
Gianluca Romanin <gianluca.romanin@vimar.com>
2014-10-02 15:56:55 +0200

    finished

--- src/predicates.clj
+++ src/predicates.clj
@@ -29,46 +29,19 @@
                (nil? %)) string))
 
 (defn has-award? [book award]
-  (let [awards (filter #(and (complement nil? %)
-                         (contains? award %)) (:awards book))]
-    (empty? awards)))
-
-(def china {:name "China Miéville", :birth-year 1972})
-(def octavia {:name "Octavia E. Butler"
-              :birth-year 1947
-              :death-year 2006})
-(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
-(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
-(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
-
-(def authors #{china, octavia, kay, dick, zelazny})
-
-(def cities {:title "The City and the City" :authors #{china}
-             :awards #{:locus, :world-fantasy, :hugo}})
-(def wild-seed {:title "Wild Seed", :authors #{octavia}})
-(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
-                    :awards #{:hugo}})
-(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
-(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
-(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
-
-(def books #{cities, wild-seed, lord-of-light,
-             deus-irae, ysabel, scanner-darkly})
-
-
-(has-award? ysabel :world-fantasy) ;=> true
-(has-award? scanner-darkly :hugo)  ;=> false
-
+  (not (nil? (award (:awards book)))))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? #(has-award? book %) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [a (map #(pred %) a-seq)
+        b (filter #(if % true false) a)]
+    (first b)))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (empty? (filter #((complement pred) %) a-seq)))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred #(=(mod n %) 0)]
+    (not (some pred (range 2 n)))))
