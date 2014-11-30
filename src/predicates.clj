4f8ebfc67dc61236e8a1a99a71736b30d1f9874a
twopisoft <twopisoft@gmail.com>
2014-11-30 20:43:13 +0800

    Set 5 Done.

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,45 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [k] (contains? a-set k)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  ((set->predicate (:awards book)) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [x] (has-award? book x)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (filter (fn [k] (or k)) (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (cond
+    (empty? a-seq) true
+    :else (nil? (my-some (complement pred) a-seq))))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [k] (== 0 (mod n k)))]
+    (not (some pred (range 2 n)))))
+
 ;^^
