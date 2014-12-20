c3311ba073b2c3880abad7305474eff4ac8772d6
Julian Jelfs <julian.jelfs@gmail.com>
2014-12-20 19:35:27 +0000

    about half way through

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,34 +1,37 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (nil? string) (empty? string) (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
+
+(defn book-has-award [book]
+  (fn [a] (has-award? book a)))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (book-has-award book) awards))
 
 (defn my-some [pred a-seq]
   :-)
