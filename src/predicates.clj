3bff723592ca341e343bcfbf5a9d5d56eb065b9a
samutamm <samutamm@gmail.com>
2014-12-23 21:03:47 +0200

    Predicate set done :)

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,50 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x)(g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [x] (< x n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== n x)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or
+   (= string nil)
+   (empty? string)
+   (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (set->predicate (:awards book)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [filtered (first (filter (fn [x] (not= x false))(map pred a-seq)))]
+    (cond
+     (= filtered nil) false
+     (= filtered true) filtered
+     :else filtered)))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [result (my-some (complement pred) a-seq)]
+    (not result)))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (= 0 (mod n x)))]
+    (not (some pred (range 2 n)))))
 ;^^
