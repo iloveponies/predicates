1d78ae83cde0fedeff77314481dfe8cc9aa8ce89
Egor Gusev <guy.do.or.die@gmail.com>
2015-01-21 02:42:50 +0300

    initial commit

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,43 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  #(< % n))
 
 (defn equal-to [n]
-  :-)
+  #(== % n))
 
 (defn set->predicate [a-set]
-  :-)
+  #(contains? a-set %))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  #(and (pred1 %) (pred2 %)))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  #(or (pred1 %) (pred2 %)))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (nil? string) 
+      (empty? string) 
+      (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? #(has-award? book %) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (or (first (filter (complement false?) (map pred a-seq))) false))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (= (count (filter (complement false?) (map pred a-seq))) (count a-seq)))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred #(= (mod n %) 0)]
+    (not (some pred (range 2 n)))))
