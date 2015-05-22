faa2b14849f165c10567b51495b214401955f046
Teemu23 <teemu.siltanen@helsinki.fi>
2015-05-22 13:48:43 +0300

    ei vielä kaikkia tehty

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,47 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n )))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (cond (every? whitespace? string) true
+        (= nil string) true
+        (= [] string) true
+        :else false))
 
 (defn has-award? [book award]
-  :-)
+  (if (contains? (:awards book) award) true false))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [x] (has-award? book x)) awards))
 
-(defn my-some [pred a-seq]
-  :-)
+(defn my-some [pred a-seq]   ; ei tehty
+  (cond (= first pred) (
+        (>= 1 (count (filter pred a-seq))) true
+        :else false)))
 
-(defn my-every? [pred a-seq]
+(defn my-every? [pred a-seq]    ; ei tehty
   :-)
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred (fn [x] (if (= 0 (mod n x))true false))]
+    (not (some pred (range 2 n)))))
+
