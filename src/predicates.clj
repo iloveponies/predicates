8ed174d8e725dd14704732b03dabf5f1576ae72a
Saqib Saleem (EE) <ssaleem@equalexperts.com>
2015-05-24 13:36:47 +0100

    Solutions to predicate - WIP

--- src/predicates.clj
+++ src/predicates.clj
@@ -13,22 +13,23 @@
   (fn [k] (contains? a-set k)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  ((set->predicate (:awards book)) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [has-award-partial? (fn [a] (has-award? book a))]
+                          (every? has-award-partial? awards)))
 
 (defn my-some [pred a-seq]
   :-)
