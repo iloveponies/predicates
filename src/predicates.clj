e2120ed8c5554694a7cac99afb179e4c9345cdd2
Robert <qberting@yahoo.com>
2015-05-25 02:08:03 -0700

    updated answers

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,47 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [a] (contains? a-set a)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [z? (fn [x] (has-award? book x))]
+  (every? z? awards)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [s (filter pred a-seq)]
+    (if (= s '())
+      nil
+      (pred (first s)))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [z (filter pred a-seq)]
+    (= z a-seq)))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (= 0 (mod n x)))]
+    (not (some pred (range 2 n)))))
 ;^^
