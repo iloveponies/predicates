a3ae8040fa048d6f6d965a4968f858ea1d322ea0
dean.witte <dean.witte@gmail.com>
2015-05-27 16:53:22 -0600

    non-prevaricating predicates

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,48 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
+
+(defn key->predicate [a-key]
+  (fn [a-map] (contains? a-map a-key)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+    (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [book-has-award? (fn [award] (has-award? book award))]
+    (every? book-has-award? awards)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [filtered (filter pred a-seq)
+        something (not (empty? filtered))]
+    (if something (pred (first filtered)))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (empty? (filter (complement pred) a-seq)))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (= 0 (mod n x)))]
+    (not (some pred (range 2 n)))))
 ;^^
