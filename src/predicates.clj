c46dcd83a75eae688a6374dda2be5de2c8017af1
ArktinenSieni <matti.raty@helsinki.fi>
2015-06-12 17:57:11 +0300

    predikaattejaa

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,47 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (> n k)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== n k)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn[x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (if (contains? book :awards)
+    (let [awards (:awards book)]
+      (contains? awards award))
+    false))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [award? (fn [award] (has-award? book award))]
+    (every? award? awards)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (some pred a-seq))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [filtered (filter (complement pred) a-seq)]
+    (empty? filtered)))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (== 0 (mod n x)))]
+    (not (some pred (range 2 n)))))
 ;^^
