2d404375afa73e4686c1435fd78c155b813f01ed
Antti Haapaniemi <antti.haapaniemi@helsinki.fi>
2015-07-21 00:41:18 +0300

    Done?

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,48 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (every? whitespace? (seq string)) (empty? string) (nil? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (get book :awards) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [filter-f (fn [award] (has-award? book award))]
+    (= (count (filter filter-f awards)) (count awards))))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [filter-f (fn [elm] (pred elm))
+    elems (filter filter-f a-seq)]
+    (if (empty? elems)
+      false
+      (pred (first elems)))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (= (count (filter (fn [x] (pred x)) a-seq)) (count a-seq)))
 
 (defn prime? [n]
-  :-)
+  (let [div (fn [x] (= (mod n x) 0))]
+    (not (some div (range 2 n)))))
+
 ;^^
