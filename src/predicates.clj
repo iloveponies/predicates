d494258f9da90aca89801e25798c1ca37fea49af
Otto <Otto@Camel.local>
2015-09-17 17:28:53 +0300

    all done

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,47 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [book-has-award? (fn [award] (has-award? book award))]
+    (every? book-has-award? awards)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [filtered-seq (filter pred a-seq)
+        mapped-seq-without-false (remove false? (map pred a-seq))]
+    (if (empty? filtered-seq)
+      false
+      (first mapped-seq-without-false))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [filtered-seq (filter pred a-seq)]
+    (= (count filtered-seq) (count a-seq))))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred (fn [x] (= (mod n x) 0))]
+    (not (some pred (range 2 n)))))
