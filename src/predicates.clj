be1c18fde5b0b6e46f9c68610bef90489d8adcd3
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2015-10-13 03:04:37 +0530

    Has-all-the-awards function not implemented yet

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,43 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [x] (< x n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== x n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (nil? string) (empty? string) (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:award book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
   :-)
 
 (defn my-some [pred a-seq]
-  :-)
+  (not (empty? (filter pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (empty? (filter (complement pred) a-seq)))
 
 (defn prime? [n]
-  :-)
+  (or (= n 2)
+      (every? false? (map (fn [x] (zero? (mod n x)))
+                                  (range 2 (+ (Math/sqrt n) 1))))))
 ;^^
