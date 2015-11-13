0b23fa199bce9e9c27f53a4fe4014fe2f9d7e705
jttait <jttait@gmail.com>
2015-11-13 21:31:00 +0000

    All tests passed

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,45 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [aw (fn [x] (has-award? book x))]
+    (every? aw awards)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (map pred (filter pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (if (empty? a-seq)
+    true
+    (if (contains? (set (map pred a-seq)) false) false true)))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [x] (= (rem n x) 0))]
+    (not (some pred (range 2 n)))))
 ;^^
