331d76d047527b12b4125140e0ba6b8633cd413f
Henrik Raitasola <henrik.raitasola@gmail.com>
2016-03-09 01:31:57 +0200

    Implemented exercises 1-11

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,43 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  #(< % n))
 
 (defn equal-to [n]
-  :-)
+  #(== % n))
 
 (defn set->predicate [a-set]
-  :-)
+  #(contains? a-set %))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x]
+    (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x]
+    (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (book :awards) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? #(has-award? book %) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (filter identity (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (empty? (filter (complement pred) a-seq)))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [remainder-integer? #(integer? (/ n %))]
+    (not (some remainder-integer? (range 2 n)))))
