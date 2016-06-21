b3949cb15d11b00500183c8c73dc5cb342da0b62
CALAMARIFRANK <sofia.blancosequeiros@helsinki.fi>
2016-06-21 17:36:13 +0300

    moi moi

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,84 @@
 (ns predicates)
 
-(defn sum-f [f g x]
-  :-)
+(defn
+  sum-f
+  [f g x]
+  (+ (f x) (g x)))
 
-(defn less-than [n]
-  :-)
+(defn
+  less-than
+  [n]
+  (fn [k] (< k n)))
 
-(defn equal-to [n]
-  :-)
 
-(defn set->predicate [a-set]
-  :-)
+(defn
+  equal-to
+  [n]
+  (fn [e] (== e n)))
 
-(defn pred-and [pred1 pred2]
-  :-)
 
-(defn pred-or [pred1 pred2]
-  :-)
+(defn
+  set->predicate
+  [a-set]
+   (fn [x]
+     (contains? a-set x)))
 
-(defn whitespace? [character]
+(defn
+  pred-and
+  [pred1 pred2]
+  (fn [x]
+    (and (pred1 x) (pred2 x))))
+
+(defn
+  pred-or
+  [pred1 pred2]
+  (fn [x]
+    (or (pred1 x) (pred2 x))))
+
+(defn
+  whitespace?
+  [character]
   (Character/isWhitespace character))
 
-(defn blank? [string]
-  :-)
+(defn
+  blank?
+  [string]
+  (cond
+    (empty? string) true
+    (nil? string) true
+    (every? whitespace? string) true
+    :else false))
 
-(defn has-award? [book award]
-  :-)
 
-(defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+(defn
+  has-award?
+  [book award]
+  (contains?
+    (:awards book) award))
 
-(defn my-some [pred a-seq]
-  :-)
+(defn
+  HAS-ALL-THE-AWARDS?
+  [book awards]
+  (every? (fn [x]
+    (has-award? book x)) awards))
 
-(defn my-every? [pred a-seq]
-  :-)
+(defn
+  my-some
+  [pred a-seq]
+  (first (map pred
+    (filter
+      (fn [x]
+        (pred x)) a-seq))))
+
+
+(defn
+  my-every?
+  [pred a-seq]
+  (== (count (filter pred a-seq))
+     (count a-seq)))
+
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [pred (fn [x]
+               (== (mod n x) 0))]
+    (not (some pred (range 2 n)))))
