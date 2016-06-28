4f9179aaf4fab86959b69b7ff5a1c54463d9ec1f
Brian Terrel <brianterrel@gmail.com>
2016-06-28 02:41:00 -0700

    Finished up predicates

--- src/predicates.clj
+++ src/predicates.clj
@@ -28,21 +28,40 @@
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (cond
+    (every? whitespace? string) true
+    (empty? string) true
+    (nil? string) true
+    :else false))
 
 (defn has-award? [book award]
-  :-)
+  (cond
+    (nil? (:awards book)) false
+    :else true))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [book-awards (:awards book)
+        check-award (fn [x] (contains? book-awards x))]
+    (if (every? check-award awards) true false)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (if (empty? (filter pred a-seq)) nil (first (map pred (filter pred a-seq)))))
 
 (defn my-every? [pred a-seq]
-  :-)
+   (if (= (filter pred a-seq) a-seq) true false))
 
 (defn prime? [n]
-  :-)
+  (let [divides-n? (fn [x] (if (= (mod n x) 0) true false))]
+    (if (empty? (filter divides-n? (range 2 n))) true false)))
 ;^^
 
+
+(my-some even? [1 3 5 7])       ;=> falsey
+(my-some even? [1 3 5 7 8])     ;=> true
+(my-some neg? [1 3 5 0 7 8])    ;=> falsey
+(my-some neg? [1 3 5 0 7 -1 8]) ;=> true
+(my-some neg? [])               ;=> falsey
+(my-some first [[false] [1]])   ;=> 1
+(my-some first [[false] []])    ;=> falsey
+(my-some nil? [1 2])            ;=> falsey
+(my-some nil? [1 nil 2])        ;=> true
