8e07139caefbad80dd24aa166d51202fcb529727
sami <sami.m.salonen@helsinki.fi>
2016-07-05 02:27:49 +0300

    5th excercise set complete

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,46 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [award] (has-award? book award)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (if (empty?  (filter pred a-seq))
+    nil
+    (first (filter boolean (map pred a-seq)))))
+
 
 (defn my-every? [pred a-seq]
-  :-)
+  (if (empty? a-seq)
+    true
+    (= (filter pred a-seq) a-seq)))
 
 (defn prime? [n]
-  :-)
+  (not (some (fn [x] (= 0 (mod n x))) (range 2 n))))
 ;^^
