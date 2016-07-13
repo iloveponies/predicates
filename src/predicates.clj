dc4e4bfbd34ac0903d6440792a3cabdb7fad8cb5
Tony Cronin <cronintony@icloud.com>
2016-07-13 08:23:05 +0100

    up to ex 5

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,34 +1,35 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (let [ fun1 f fun2 g]
+   (+ (fun1 x) (fun2 x))))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
-(defn blank? [string]
-  :-)
+(defn blank? [string] 
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  ((set->predicate (:awards book)) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? true? (map (fn [x] (has-award? book x)) awards)))
 
 (defn my-some [pred a-seq]
   :-)
