076c1758c10341c9f4616007e7645c7c664e36b5
Summer Babe <theopenmind21@gmail.com>
2017-01-15 19:39:49 +0200

    the predicates are not predictive

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,76 @@
 (ns predicates)
 
-(defn sum-f [f g x]
-  :-)
+(defn sum-f
+  "Take two functions and an argument, apply both function to an argument, and return the result."
+  [function-first function-second argument]
+  (+ (function-first argument) (function-second argument)))
 
-(defn less-than [n]
-  :-)
+(defn less-than
+  "Create a predicate, which squeezes out values which exceed given number, from a collection,
+  returning a collection of items, which are less than upper limit."
+  [upper-limit]
+  (fn [tale] (< tale upper-limit)))
 
-(defn equal-to [n]
-  :-)
+(defn equal-to
+  "Create a predicate, which evaluates as truthy if a-set contains given value.."
+  [fairy]
+  (fn [tale] (== fairy tale)))
 
-(defn set->predicate [a-set]
-  :-)
+(defn set->predicate
+  "Create a predicate, which evaluates as truthy,
+   if a-set contains given value, and falsey otherwise."
+  [a-set]
+  (fn [value] (contains? a-set value)))
 
-(defn pred-and [pred1 pred2]
-  :-)
+(defn pred-and
+  "Create a predicate, which evaluates as truthy,
+   when both argument predicates evaluate as truthy."
+  [predicate-first predicate-second]
+  (fn [tale] (and (predicate-first tale) (predicate-second tale))))
 
-(defn pred-or [pred1 pred2]
-  :-)
+(defn pred-or
+  "Create a predicate, which evaluates as truthy,
+  when at least one of argument predicates evaluate as truthy."
+  [predicate-first predicate-second]
+  (fn [tale] (or (predicate-first tale) (predicate-second tale))))
 
-(defn whitespace? [character]
+(defn whitespace?
+  "Detect, if this character is whitespace!"
+  [character]
   (Character/isWhitespace character))
 
-(defn blank? [string]
-  :-)
+(defn blank?
+  "Find out, whether this string is blank!"
+  [string]
+  (every? whitespace? string)
+  )
 
-(defn has-award? [book award]
-  :-)
+(defn has-award?
+  "Detect, if this book has received a given award."
+  [book award]
+  (contains? (get book :awards) award))
 
-(defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+(defn HAS-ALL-THE-AWARDS?
+  "Detect, if this book has all the awards in a set."
+  [book awards]
+  (every? (fn [award] (has-award? book award)) awards))
 
-(defn my-some [pred a-seq]
-  :-)
+(defn my-some
+  "My very special some implementation."
+  [pred a-seq]
+  (first (filter boolean (map pred a-seq))))
 
-(defn my-every? [pred a-seq]
-  :-)
+(defn my-every?
+  "My very special every? implementation."
+  [pred a-seq]
+  (empty? (filter (complement pred) a-seq)))
 
-(defn prime? [n]
-  :-)
-;^^
+(defn prime?
+  "Determine, if given number a prime."
+  [tale]
+  (let
+    [evenly-divisible (fn [num] (== 0 (mod tale num)))
+     progression (range 1 tale)
+     ] (= (seq [1]) (filter evenly-divisible progression))))
+
+;^__^
