1e9b4283c6d889718719a1555c64c61780b0b4ad
masilven <silvennoinen.markus@gmail.com>
2017-03-05 13:08:18 +0200

    Predicates - Done

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,51 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== n k)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (nil? string) (empty? string) (every? whitespace? string)))
 
 (defn has-award? [book award]
-  :-)
+  (if (not (nil? (:awards book)))
+    (contains? (:awards book) award)
+    false))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (fn [award] (has-award? book award)) awards))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [filtered (first (filter pred a-seq))]
+    (if ((complement nil?) filtered)
+      (pred filtered)
+      (and (= pred nil?) (not (every? (complement nil?) a-seq))))))
+; as contains? could not find nil, I made a workaround
+      
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [len1 (count a-seq)
+        len2 (count (filter pred a-seq))]
+    (== len1 len2)))
 
 (defn prime? [n]
-  :-)
+  (let [pred (fn [k] (== 0 (mod n k)))]
+    (not (some pred (range 2 n)))))
 ;^^
