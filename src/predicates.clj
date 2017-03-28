0d36dbcaa549ffb848d009b801be4f46df45c157
Sebastian Björkqvist <sebastian@sebastianbjorkqvist.com>
2017-03-28 16:34:38 +0300

    sum-f, less-than and equal-to implemented

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,13 +1,13 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
   :-)
