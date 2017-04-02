f8b9490c441e55979752fc7839230eb3c53a6120
Tuomas Lappeteläinen <tuomas.lappetelainen@gmail.com>
2017-04-02 22:30:48 +0300

    implement three first exercises

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject predicates "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [iloveponies.tests/predicates "0.1.0-SNAPSHOT"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
