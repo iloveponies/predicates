d9d3a3584d7dcbb2de53789728c6a263e4170ff0
Tuomas Lappeteläinen <tuomas.lappetelainen@gmail.com>
2017-04-05 23:11:20 +0300

    improve my-every? a bit

--- src/predicates.clj
+++ src/predicates.clj
@@ -37,9 +37,7 @@
   (first (filter #(boolean %) (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  ;; This is a bit ugly..
-  (== (count a-seq)
-      (count (filter #(boolean %) (map pred a-seq)))))
+  (not (my-some (complement pred) a-seq)))
 
 (defn prime? [n]
   (let [pred (fn [x] (== (mod n x) 0))]
