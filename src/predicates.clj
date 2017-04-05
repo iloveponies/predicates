8aeb5f35ce444e2e242b28ed9b111c2aa8a4f8ee
Samuli Kärki <samuli.karki@eniram.fi>
2017-04-05 14:13:14 +0300

    Finish some exercises

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,16 +1,16 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] ()))
 
 (defn pred-and [pred1 pred2]
   :-)
