588d6110944ca2bd797ff17cb84f4d8878980efe
Miika Pärni <miika.parni@helsinki.fi>
2017-05-13 11:07:15 +0300

    predicates done?

--- src/predicates.clj
+++ src/predicates.clj
@@ -21,26 +21,43 @@
 )
 
 (defn pred-or [pred1 pred2]
-  :-)
+   (fn [x] (or (pred2 x) (pred1 x)))
+)
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+(every? whitespace? string)
+)
 
 (defn has-award? [book award]
-  :-)
+(cond
+  (:awards book) true
+  :else false)
+)
+
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (cond
+    (= 0 (count awards)) true
+    (= 1 (count awards)) (has-award? book awards)
+    :else  (= awards (:awards book)))
+)
 
 (defn my-some [pred a-seq]
-  :-)
+  (some pred a-seq)
+  ; (not (empty? (filter pred a-seq)))
+)
 
 (defn my-every? [pred a-seq]
-  :-)
+  (cond
+    (= 0 (first (sort a-seq))) false
+    :else true)
+)
 
 (defn prime? [n]
-  :-)
+  (empty? (filter #(= 0 (mod n %)) (range 2 n)))
+)
+
 ;^^
