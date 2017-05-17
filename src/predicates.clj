4ca7e3fc7c3fb80b75363ba219e69ee43bc73146
Sean R. Lang <srlang@ncsu.edu>
2017-05-17 19:33:29 +0300

    srlang: submission

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,51 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [x] (< x n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [x] (== x n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (or (every? whitespace? string) (empty? string)))
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
-
-(defn my-some [pred a-seq]
-  :-)
+  (every? (fn[x] (has-award? book x)) awards))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (empty? (filter (complement pred) a-seq)))
 
 (defn prime? [n]
-  :-)
-;^^
+  (let [poss-factors (range 2 n)
+  ;(+ 1 (Math/sqrt n))) ; smarter factorization (answer as (...) not [...])
+        pred? (fn [x] (== 0 (mod n x)))]
+    (not (some pred? poss-factors))))
+
+;;;;;;;;; DONE ABOVE ;;;;;;;;;
+
+(defn my-some [pred a-seq]
+  ;(pred
+    (first
+      (filter pred a-seq)
+    )
+  ;)
+)
+  ;(first (filter pred a-seq)))
