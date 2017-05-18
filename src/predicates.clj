a37af35ea819b7310b2d29e529a1d995ae0fb2a9
Väinö Niemi <vaino.a.niemi@gmail.com>
2017-05-18 15:01:24 +0300

    Exercise 1 and 2 done.

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,13 +1,19 @@
 (ns predicates)
 
-(defn sum-f [f g x]
-  :-)
+(defn
+  sum-f
+  [f g x]
+  (+ (f x) (g x)))
 
-(defn less-than [n]
-  :-)
+(defn
+  less-than
+  [n]
+  (fn [k] (< k n)))
 
-(defn equal-to [n]
-  :-)
+(defn
+  equal-to
+  [n]
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
   :-)
