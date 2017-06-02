3cc2f20a8c41670b420187c600d726efca7531db
Riikka Taiminen <riikka.taiminen@solita.fi>
2017-06-02 15:38:32 +0300

    predicates done

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,41 +1,51 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
 
 (defn has-award? [book award]
-  :-)
+  (let [awards (:awards book)]
+    (contains? awards award)))
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (let [has-book-award? (fn [x] (has-award? book x))]
+    (every? has-book-award? awards)))
 
 (defn my-some [pred a-seq]
-  :-)
+  (let [first-non-false (first (filter (complement false?) (map pred a-seq)))]
+    (if (nil? first-non-false)
+    false
+    first-non-false)))
 
 (defn my-every? [pred a-seq]
-  :-)
+  (let [first-non-true (first (filter (complement true?) (map pred a-seq)))]
+    (if (nil? first-non-true)
+      true
+      false)))
 
 (defn prime? [n]
-  :-)
+  (let [pred #(= (mod n %) 0)]
+    (not (some pred (range 2 n)))))
+
 ;^^
