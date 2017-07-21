c44d278576af85cedd8bf0d6cfcf6deb04547e21
wang26754 <26664179+wang26754@users.noreply.github.com>
2017-07-21 13:45:12 +0800

    Update predicates.clj

--- src/predicates.clj
+++ src/predicates.clj
@@ -4,16 +4,16 @@
   (+ (f x) (g x)))
 
 (defn less-than [n]
-  (fn [k] (< k n)))
+  (fn [x] (< x n)))
 
 (defn equal-to [n]
-  (fn [k] (== k n)))
+  (fn [x] (== x n)))
 
 (defn set->predicate [a-set]
   (fn [x] (contains? a-set x)))
 
 (defn pred-and [pred1 pred2]
-  (fn [x] (or (pred1 x) (pred2 x))))
+  (fn [x] (and (pred1 x) (pred2 x))))
 
 (defn pred-or [pred1 pred2]
   (fn [x] (or (pred1 x) (pred2 x))))
@@ -22,16 +22,16 @@
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  (every? whitespace? string))
-
-(defn has-award? [book award]
- (contains? (:awards book) award))
+  (or (every? whitespace? string) (empty? string)))
 
 (defn has-award? [book award]
   (contains? (:awards book) award))
 
+(defn HAS-ALL-THE-AWARDS? [book awards]
+  (every? (fn[x] (has-award? book x)) awards))
+
 (defn my-some [pred a-seq]
-  (filter (complement false?) (map pred a-seq)))
+  (first (filter (complement false?) (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
   (empty? (filter (complement pred) a-seq)))
