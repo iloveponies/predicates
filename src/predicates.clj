e09d11ba6251232153425b74d216c153e0a3f219
Kalle Myllymaa <kalle.myllymaa@gmail.com>
2017-07-23 20:55:13 +0300

    progress, baby!

--- src/predicates.clj
+++ src/predicates.clj
@@ -1,37 +1,47 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x))
+)
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n))
+)
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n))
+)
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [a-value] (contains? a-set a-value))
+)
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x)))
+)
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x)))
+)
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string)
+)
 
 (defn has-award? [book award]
-  :-)
+  (contains? (:awards book) award)
+)
 
 (defn HAS-ALL-THE-AWARDS? [book awards]
-  :-)
+  (every? (:awards book) awards)
+)
 
 (defn my-some [pred a-seq]
-  :-)
+  (first (map pred (filter pred a-seq)))
+)
 
 (defn my-every? [pred a-seq]
   :-)
