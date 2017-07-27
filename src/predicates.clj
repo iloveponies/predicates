b4b90eb1157c8bd4673fc28f9aff657beb73a2b7
Han Lin <e1001924@gmail.com>
2017-07-27 21:37:07 +0200

    again commit

--- src/predicates.clj
+++ src/predicates.clj
@@ -45,6 +45,6 @@
     :else false))
 
 (defn prime? [n]
-  (let [f1 (fn [x] (mod n x))]
-    (if(== (count (filter (equal-to 0) (map f1 (range 2 n)))) 0) true false)))
-;^^^
+  (let [f1 (fn [x] (== (mod n x) 0))]
+    (not (some f1 (range 2 n)))))
+;^^
