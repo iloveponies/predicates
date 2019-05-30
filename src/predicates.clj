3d8323323c49222b5fa9e8bcd5dc3d0b7f8b657b
teo.mertanen <teo.mertanen@gofore.com>
2019-05-30 17:13:13 +0300

    Exercises 4-5

--- src/predicates.clj
+++ src/predicates.clj
@@ -13,10 +13,16 @@
   (fn [a-key] (contains? a-set a-key)))
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x]
+    (and
+      (pred1 x)
+      (pred2 x))))
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x]
+    (or
+      (pred1 x)
+      (pred2 x))))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
