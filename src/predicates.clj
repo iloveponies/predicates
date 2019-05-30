79a7c92660f95ec59b80ecb3b825486eb93e2088
teo.mertanen <teo.mertanen@gofore.com>
2019-05-30 17:57:11 +0300

    fix my-some

--- src/predicates.clj
+++ src/predicates.clj
@@ -39,10 +39,10 @@
   (every? (fn [a] (has-award? book a)) awards))
 
 (defn my-some [pred a-seq]
-  (first (filter pred a-seq)))
+  (first (filter (fn [x] x) (map pred a-seq))))
 
 (defn my-every? [pred a-seq]
-  (empty? (filter (complement pred) a-seq)))
+  (empty? (filter (complement pred a-seq)))
 
 (defn prime? [n]
   :-)
